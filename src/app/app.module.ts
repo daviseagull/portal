3f8feeac258a2945aa88a7634fe77f5e5b09d7c7
Davi Seagull <davi.seagull@gmail.com>
2022-03-14 20:50:27 -0300

    feat: adding navbar, header, frotas, rotas and homepage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { FrotasComponent } from './frotas/frotas.component';
+import { RotasComponent } from './rotas/rotas.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent, FooterComponent, HomepageComponent, FrotasComponent, RotasComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
