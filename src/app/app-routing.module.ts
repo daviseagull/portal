0e27384e639b11463d2849b675a1eb783d7039d5
Davi Seagull <davi.seagull@gmail.com>
2022-03-15 14:11:32 -0300

    feat: adding homepage to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { FrotasComponent } from './frotas/frotas.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { RotasComponent } from './rotas/rotas.component';
 
 const routes: Routes = [
-  {path: 'homepage', component: HomepageComponent},
-  {path: 'frotas', component: FrotasComponent},
-  {path: 'rotas', component: RotasComponent}
+  { path: '#', component: AppComponent },
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'frotas', component: FrotasComponent },
+  { path: 'rotas', component: RotasComponent },
 ];
 
 @NgModule({
