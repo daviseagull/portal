35ac72d40d88b5792a19b096d254040cf00afc61
Davi Seagull <davi.seagull@gmail.com>
2022-03-16 16:19:03 -0300

    feat: adding frotas form and styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { RoutesComponent } from './routes/routes.component';
-import { VehiclesComponent } from './vehicles/vehicles.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { RoutesComponent } from './components/routes/routes.component';
+import { VehicleFormComponent } from './components/vehicles/vehicle-form/vehicle-form.component';
+import { VehiclesComponent } from './components/vehicles/vehicles.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'vehicles', component: VehiclesComponent },
+  { path: 'vehicle-form', component: VehicleFormComponent },
   { path: 'routes', component: RoutesComponent },
 ];
 
